58236b219c2c704c0164c645b649b8000e0c28b2
abdulahadabduvahobov75@gmail.com <abdulahadabduvahobov75@gmail.com>
2023-09-26 18:45:12 +0500

    hfbdfhj

--- wordle.js
+++ wordle.js
@@ -104,7 +104,7 @@ function intialize() {
 
   let keyboard = [
     ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-    ["A", "S", "D", "F", "G", "H", "J", "K", "L", ""],
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L", "'"],
     ["Enter", "Z", "X", "C", "V", "B", "N", "M", "⌫"],
   ];
 
